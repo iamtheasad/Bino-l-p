9ec293b34f9559b71264728f5c6e4ddc22ed46ce
iamtheasad <iamtheasad@gmail.com>
2020-01-10 12:05:02 +0600

    header-section slcik carousel added fixed some issues /

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $('.header_slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows:true,
+      prevArrow: '<button type="button" class="slick-prev"> <i class="fas fa-chevron-left"></i> </button>',
+      nextArrow: '<button type="button" class="slick-next"> <i class="fas fa-chevron-right"></i> </button>',
+      draggable: false,
+      fade: true,
+   });
+ });
