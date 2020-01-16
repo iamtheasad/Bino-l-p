aea31476f6d3630f44770fae50881e7aa157f4e8
iamtheasad <iamtheasad@gmail.com>
2020-01-16 16:44:13 +0600

    service-section added with hover effect

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 // Header Slider
-$(document).ready(function(){
+$(document).ready(function () {
    $('.header_slider').slick({
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
-      arrows:true,
+      arrows: true,
       prevArrow: '<button type="button" class="slick-prev"> <i class="fas fa-chevron-left"></i> </button>',
       nextArrow: '<button type="button" class="slick-next"> <i class="fas fa-chevron-right"></i> </button>',
       draggable: false,
       fade: true,
    });
- });
+});
 
 //  Testimonial Slider
-$(document).ready(function(){
+$(document).ready(function () {
    $('.testimonial_area').slick({
       infinite: true,
       slidesToShow: 4,
@@ -24,4 +24,47 @@ $(document).ready(function(){
       autoplaySpeed: 2000,
       arrows: false,
    });
+});
+
+// Services Section Hover Effect
+$(document).ready(function () {
+
+   $(".service_icon_1, .service_text_1").hover(function () {
+      $(".service_bar_1").css({
+         "background-color": "#e74c3c",
+         "height": "2px"
+      });
+   }, function () {
+      $(".service_bar_1").css({
+         "background-color": "#2a2a2a",
+         "height": "1px"
+      });
+   });
+
+
+   $(".service_icon_2, .service_text_2").hover(function () {
+      $(".service_bar_2").css({
+         "background-color": "#e74c3c",
+         "height": "2px"
+      });
+   }, function () {
+      $(".service_bar_2").css({
+         "background-color": "#2a2a2a",
+         "height": "1px"
+      });
+   });
+
+
+   $(".service_icon_3, .service_text_3").hover(function () {
+      $(".service_bar_3").css({
+         "background-color": "#e74c3c",
+         "height": "2px"
+      });
+   }, function () {
+      $(".service_bar_3").css({
+         "background-color": "#2a2a2a",
+         "height": "1px"
+      });
+   });
+
 });
