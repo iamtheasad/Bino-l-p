c9b78beb8d8dd4c2b3dde15f1251ef56ca5eec1a
iamtheasad <iamtheasad@gmail.com>
2020-01-29 21:35:17 +0600

    On Read More Modal added, top button added & some fixes

--- js/script.js
+++ js/script.js
@@ -1,163 +1,202 @@
-$(document).ready(function () {
+(function iamScript() {
+   "use strict";
 
-   // Header Slider
-   $('.header_slider').owlCarousel({
-      loop: true,
-      items: 1,
-      nav: true,
-      dots: false,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      animateIn: 'bounceInRight',
-      animateOut: 'bounceOutLeft',
-      navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-   });
+   $(document).ready(function () {
+
+      // Header Slider
+      $('.header_slider').owlCarousel({
+         loop: true,
+         items: 1,
+         nav: true,
+         dots: false,
+         autoplay: true,
+         autoplaySpeed: 2000,
+         animateIn: 'bounceInRight',
+         animateOut: 'bounceOutLeft',
+         navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+      });
 
 
-   //  Testimonial Slider
-   $('.testimonial_area').owlCarousel({
-      loop: true,
-      autoplay: true,
-      responsive: {
-         0: {
-            items: 1
-         },
-         600: {
-            items: 2
-         },
-         1000: {
-            items: 4
+      //  Testimonial Slider
+      $('.testimonial_area').owlCarousel({
+         loop: true,
+         autoplay: true,
+         responsive: {
+            0: {
+               items: 1
+            },
+            600: {
+               items: 2
+            },
+            1000: {
+               items: 4
+            }
          }
-      }
-   });
-
-
-   /* Services Section Hover Effect Start */
-   $(".service_icon_1, .service_text_1").hover(function () {
-      $(".service_bar_1").css({
-         "background-color": "#e74c3c",
-         "height": "2px"
       });
-   }, function () {
-      $(".service_bar_1").css({
-         "background-color": "#2a2a2a",
-         "height": "1px"
+
+
+      /* Services Section Hover Effect Start */
+      $(".service_icon_1, .service_text_1").hover(function () {
+         $(".service_bar_1").css({
+            "background-color": "#e74c3c",
+            "height": "2px"
+         });
+      }, function () {
+         $(".service_bar_1").css({
+            "background-color": "#2a2a2a",
+            "height": "1px"
+         });
       });
-   });
 
 
-   $(".service_icon_2, .service_text_2").hover(function () {
-      $(".service_bar_2").css({
-         "background-color": "#e74c3c",
-         "height": "2px"
+      $(".service_icon_2, .service_text_2").hover(function () {
+         $(".service_bar_2").css({
+            "background-color": "#e74c3c",
+            "height": "2px"
+         });
+      }, function () {
+         $(".service_bar_2").css({
+            "background-color": "#2a2a2a",
+            "height": "1px"
+         });
       });
-   }, function () {
-      $(".service_bar_2").css({
-         "background-color": "#2a2a2a",
-         "height": "1px"
+
+
+      $(".service_icon_3, .service_text_3").hover(function () {
+         $(".service_bar_3").css({
+            "background-color": "#e74c3c",
+            "height": "2px"
+         });
+      }, function () {
+         $(".service_bar_3").css({
+            "background-color": "#2a2a2a",
+            "height": "1px"
+         });
       });
-   });
+      /* Services Section Hover Effect End */
 
 
-   $(".service_icon_3, .service_text_3").hover(function () {
-      $(".service_bar_3").css({
-         "background-color": "#e74c3c",
-         "height": "2px"
-      });
-   }, function () {
-      $(".service_bar_3").css({
-         "background-color": "#2a2a2a",
-         "height": "1px"
-      });
-   });
-   /* Services Section Hover Effect End */
-
-
-   /* Recent Works Isotope Start */
-   var $grid = $('.iso_recent_work').isotope({
-      itemSelector: '.recent_work_box',
-      percentPosition: true,
-      masonry: {
-         // use outer width of grid-sizer for columnWidth
-         columnWidth: 1
-      }
-   });
-
-   // filter items on button click
-   $('.recent_work_menu').on('click', 'button', function () {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({
-         filter: filterValue
-      });
-   });
-
-   // Active Class For Isotope Menu
-   $('.recent_work_menu button').on('click', function (e) {
-      $(this).addClass('recent_work_active').siblings('.recent_work_active').removeClass('recent_work_active');
-      e.preventDefault();
-   });
-   /* Recent Works Isotope End */
-
-
-   // Recent-Works Slider
-   $(".recent_work_slider").owlCarousel({
-      items: 1,
-      loop: true,
-      autoplay: true,
-      animateIn: 'animateIn',
-      animateOut: 'bounceOut',
-   });
-
-
-   // Statistics Counterup
-   // Source - https://www.jqueryscript.net/other/Counting-Up-To-Numerical-Values-On-Scroll-jQuery-Countup-js.html
-   $(".statistics_countup").countUp({
-      'time': 3000,
-      'delay': 100,
-   });
-
-
-   // Pricing Card Slider With Owl Carousel
-   $(".pricing_card_slider").owlCarousel({
-      loop: true,
-      autoplay: true,
-      nav: false,
-      dots: false,
-      animateIn: 'animateIn',
-      animateOut: 'animateOut',
-      responsive: {
-         0: {
-            items: 1,
-         },
-         600: {
-            items: 2,
-         },
-         1000: {
-            items: 3
+      /* Recent Works Isotope Start */
+      var $grid = $('.iso_recent_work').isotope({
+         itemSelector: '.recent_work_box',
+         percentPosition: true,
+         masonry: {
+            // use outer width of grid-sizer for columnWidth
+            columnWidth: 1
          }
-      }
-   });
+      });
+
+      // filter items on button click
+      $('.recent_work_menu').on('click', 'button', function () {
+         var filterValue = $(this).attr('data-filter');
+         $grid.isotope({
+            filter: filterValue
+         });
+      });
+
+      // Active Class For Isotope Menu
+      $('.recent_work_menu button').on('click', function (e) {
+         $(this).addClass('recent_work_active').siblings('.recent_work_active').removeClass('recent_work_active');
+         e.preventDefault();
+      });
+      /* Recent Works Isotope End */
 
 
-   //   Integratin Section Owl Carousel
-   $('.integration_item').owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: false,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      dots: false,
-      responsive: {
-         0: {
-            items: 1
-         },
-         600: {
-            items: 3
-         },
-         1000: {
-            items: 4
+      // Recent-Works Slider
+      $(".recent_work_slider").owlCarousel({
+         items: 1,
+         loop: true,
+         autoplay: true,
+         animateIn: 'animateIn',
+         animateOut: 'bounceOut',
+      });
+
+
+      // Statistics Counterup
+      // Source - https://www.jqueryscript.net/other/Counting-Up-To-Numerical-Values-On-Scroll-jQuery-Countup-js.html
+      $(".statistics_countup").countUp({
+         'time': 3000,
+         'delay': 100,
+      });
+
+
+      // Pricing Card Slider With Owl Carousel
+      $(".pricing_card_slider").owlCarousel({
+         loop: true,
+         autoplay: true,
+         nav: false,
+         dots: false,
+         animateIn: 'animateIn',
+         animateOut: 'animateOut',
+         responsive: {
+            0: {
+               items: 1,
+            },
+            600: {
+               items: 2,
+            },
+            1000: {
+               items: 3
+            }
          }
-      }
-   });
+      });
 
-});
+
+      //   Integratin Section Owl Carousel
+      $('.integration_item').owlCarousel({
+         loop: true,
+         margin: 10,
+         nav: false,
+         autoplay: true,
+         autoplaySpeed: 1000,
+         dots: false,
+         responsive: {
+            0: {
+               items: 1
+            },
+            600: {
+               items: 3
+            },
+            1000: {
+               items: 4
+            }
+         }
+      });
+
+
+      /* Scroll to Top Button Start */
+
+      // https://codepen.io/karencho/pen/KIgur
+      // declare variable
+      var topBottomButton = $(".top_bottom_button");
+
+      $(window).scroll(function () {
+
+         // Declare Variable
+         var topPosition = $(this).scrollTop(); // scrollTop is a jquery function
+
+         // if user scrolls down - show scroll to top button
+         if (topPosition > 900) {
+            $(topBottomButton).css({
+               "opacity": "1",
+            })
+
+         } else {
+            $(topBottomButton).css({
+               "opacity": "0",
+            })
+         }
+      }); // Scroll End
+
+      //Click event to scroll to top
+      $(topBottomButton).on('click', function () {
+         $('html, body').animate({
+            scrollTop: 0
+         }, 5000);
+         return false;
+
+      }); // click() scroll top end
+      /* Scroll to Top Button End */
+
+   }); // ready() function end
+})(); // "use strict" method end
